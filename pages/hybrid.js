9366db4d3ccede88bf957a3c3c602c4fe236eed7
Jurn van Wissen <jurnvanwissen@gmail.com>
2021-07-19 19:06:26 +0700

    initial commit

---
+++ pages/hybrid.js
@@ -0,0 +1,30 @@
+import { useAmp } from "next/amp";
+import Image from "next/image";
+
+export const config = { amp: "hybrid" };
+
+export default function Hybrid() {
+  const isAmp = useAmp();
+
+  return (
+    <div>
+      <h1>Hybrid AMP Page</h1>
+
+      {isAmp ? (
+        <amp-img
+          alt="A view of the sea"
+          src="/chiangmai.jpeg"
+          width="640"
+          height="360"
+        ></amp-img>
+      ) : (
+        <Image
+          alt="A view of the sea"
+          src="/chiangmai.jpeg"
+          width="640"
+          height="360"
+        />
+      )}
+    </div>
+  );
+}
